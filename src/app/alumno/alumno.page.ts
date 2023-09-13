ee6d83a32783b201e17765716914f1bc5ca77d4c
NicoSabandoff <ni.sabando@duocuc.cl>
2023-09-13 00:46:12 -0300

    compadre

--- src/app/alumno/alumno.page.ts
+++ src/app/alumno/alumno.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+import { ActivatedRoute, Router, NavigationExtras } from "@angular/router";
 
 @Component({
   selector: 'app-alumno',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alumno.page.scss'],
 })
 export class AlumnoPage implements OnInit {
+  userHome = "";
+  userName = "";
 
-  constructor() { }
 
+  constructor(private activeroute: ActivatedRoute,
+    private actionSheetCtrl: ActionSheetController,
+
+    private router: Router
+  ) {
+
+    this.activeroute.queryParams.subscribe(params => {
+      if (this.router.getCurrentNavigation()?.extras.state) {
+        this.userHome = this.router.getCurrentNavigation()?.extras.state?.['user'];
+        this.userName = this.router.getCurrentNavigation()?.extras.state?.['name'];
+      }
+    });
+  }
   ngOnInit() {
   }
 
-}
+  registrarAsistencia() {
+  }
+  }
